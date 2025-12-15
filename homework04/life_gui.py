7e25377776fa0d763afee0aa114534832be1f29b
PhoxinuSi <galyanovs1506@gmail.com>
2025-12-15 21:20:42 +0300

    исправляем isort

--- homework04/life_gui.py
+++ homework04/life_gui.py
@@ -1,8 +1,10 @@
-import pygame
-from life import GameOfLife
-from pygame.locals import *
-from ui import UI
 from pathlib import Path
+
+import pygame
+from pygame.locals import *
+
+from life import GameOfLife
+from ui import UI
 
 
 class GUI(UI):
